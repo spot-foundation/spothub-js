7011822cd5808464eaeec1c86d90bcd12ea1f947
Foy Savas <foy@sav.as>
2021-12-03 16:51:53 -0500

    use attrs for apiPathFor

--- spotspace/index.test.js
+++ spotspace/index.test.js
@@ -10,6 +10,7 @@ test("spotspace", async () => {
   const ss = spotspace({prefixUrl});
   const { Space, SpaceLedgerEntry } = ss.models;
   const space0 = Space.build({name: 'hello'});
+  console.log(space0);
   await space0.save();
   const space0found = await Space.findOne({where: {id: space0.id}});
   expect(space0found).not.toBe(null);
--- testlib/mockapi/spaces/get.js
+++ testlib/mockapi/spaces/get.js
@@ -1,6 +1,6 @@
 export default async function (req, res) {
   let space = null;
-  const where = JSON.parse(req.query.where);
+  const where = req.query.where ? JSON.parse(req.query.where) : {};
   if (req.query.limit == 1) {
     space = globalThis.MockData.spaces[where.id];
   }
