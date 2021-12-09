efa92a65543b384f4716d412bc664bf5fcb20afa
Foy Savas <foy@sav.as>
2021-12-09 18:42:57 -0500

    rename to spothub-js

---
+++ spothub/index.js
@@ -0,0 +1,18 @@
+import Space from "../lib/Space";
+import Ledger from "../lib/Ledger";
+import LedgerEntry from "../lib/LedgerEntry";
+
+const setModelConnection = function (aModel, connection) {
+  aModel.connection = connection;
+  return aModel;
+};
+
+export default function spothub(config) {
+  const connection = config;
+  const models = {
+    Space: setModelConnection(Space, connection),
+    Ledger: setModelConnection(Ledger, connection),
+    LedgerEntry: setModelConnection(LedgerEntry, connection),
+  };
+  return { connection, models };
+}
